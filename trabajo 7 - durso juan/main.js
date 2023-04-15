7bd376a3c86133ef7239e4ee731e9416991d375e
juandurso <juandurso1@gmail.com>
2023-04-15 15:25:17 -0300

    agrego tp 7

---
+++ trabajo 7 - durso juan/main.js
@@ -0,0 +1,22 @@
+// Crear un script que luego de 3 segundos muestre en el navegador la fecha del día.
+
+
+// function mostrarFecha (){
+//     let fecha = new Date()
+//     document.write(fecha.toLocaleDateString())
+// }
+
+// setTimeout(mostrarFecha, 3000)
+
+// Con 30 minutos de bicicleta puedes quemar 192 calorías. Crear un script que muestre en consola las calorías que vas quemando por segundo simulando que vas en bicicleta
+
+// let caloriasPorSegundo = 192 / (30*60)
+
+// let caloriasQuemadas = 0
+
+// function actualizarCalorias (){
+//     caloriasQuemadas += caloriasPorSegundo
+//     console.log("calorias quemadas: " + caloriasQuemadas.toFixed(2))
+// }
+
+// let intervalo = setInterval(actualizarCalorias, 1000)
